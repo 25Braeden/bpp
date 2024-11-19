338a498ec86fa3a43806a048cf3bdcbd2f5ec820
25braeden <25baudlin@msjnet.net>
2024-11-19 11:00:01 -0500

    Add navigation functionality and improve slide layout

---
+++ src/scripts.js
@@ -0,0 +1,30 @@
+function go(direction) {
+    // Get the current file name from the URL
+    const currentFile = window.location.pathname.split('/').pop();
+    
+    // Determine the current slide number
+    let currentSlideNumber;
+    if (currentFile === 'index.html') {
+        currentSlideNumber = 1;
+    } else {
+        currentSlideNumber = parseInt(currentFile.match(/\d+/)[0]);
+    }
+
+    // Calculate the new slide number
+    const newSlideNumber = currentSlideNumber + direction;
+
+    // Generate the new file name
+    let newFile;
+    if (newSlideNumber === 1) {
+        newFile = 'index.html';
+    } else if (newSlideNumber < 1) {
+        // Handle case where newSlideNumber is less than 1
+        alert("You are already on the first slide.");
+        return;
+    } else {
+        newFile = `slide${newSlideNumber}.html`;
+    }
+
+    // Navigate to the new file
+    window.location.href = newFile;
+}
